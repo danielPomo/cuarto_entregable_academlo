595b6f0223075339403d5069343e1110d41909c7
danielPomo <danielnicolaspomo@gmail.com>
2023-05-26 01:15:31 -0300

    I sync the db

--- src/app.js
+++ src/app.js
@@ -1,6 +1,10 @@
 require('dotenv').config()
 const express = require('express')
 const db = require('./utils/database')
+const Users = require('./models/users.model.js')
+const Messages = require('./models/messages.model.js')
+const ChatTypes = require('./models/chatTypes.model.js')
+const Chats = require('./models/chats.model.js')
 const PORT = process.env.PORT || 8000
 
 const app = express()
@@ -13,6 +17,9 @@ app.get('/', (req, res) => {
 // .then( () => {console.log("La base de datos se ha conectado de modo exitoso")})
 // .catch( (err) => {console.log(err)} )
 
+db.sync()
+.then( () => console.log('La base de datos se ha sincronizado con exito') )
+.catch( (err) => console.log(err) )
 
 
 app.listen(PORT, () => {
---
+++ src/models/initModels.js
@@ -0,0 +1,8 @@
+// const Users = require('./users.model.js')
+// const Messages = require('./messages.model.js')
+// const ChatTypes = require('./chatTypes.model.js')
+// const Chats = require('./chats.model.js')
+
+// const initModels = () => {
+    
+// }
